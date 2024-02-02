c4715f9d102962c70310e809c06e187e75ac24c8
Craacky <cbxj.ysa@gmail.com>
2024-02-02 13:21:29 +0300

    *wip

--- lab1/src/main/java/org/example/Main.java
+++ lab1/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -33,49 +35,36 @@ public class Main {
 
         FileInputStream fis = new FileInputStream(file);
         BufferedImage image = ImageIO.read(file);
-
-        byte[] header = new byte[54];
-        fis.read(header);
-
-        int colorsCount = 0;
-        for (int y = 0; y < image.getHeight(); y++) {
-            for (int x = 0; x < image.getWidth(); x++) {
-                int rgb = image.getRGB(x, y);
-                if (rgb != 0) colorsCount++;
-            }
-
-        }
-
-        double fileSize = byteArrayToInt(header, 2);
-        int width = byteArrayToInt(header, 18);
-        int height = byteArrayToInt(header, 22);
-        int bitsPerPixel = byteArrayToInt(header, 28);
-        int compression = image.getType();
-
+        
         System.out.println("File type: bitmap(BMP)");
-        System.out.println("Value of file: " + fileSize / (1024L * 1024L) + " MB");
-        System.out.println("Width of file: " + width + " pixels");
-        System.out.println("Height of file: " + height + " pixels");
-        System.out.println("BitsPerPixel: " + bitsPerPixel + " bits");
+        System.out.println("Value of file: " + ((double) file.length() / (1024L * 1024L)) + " MB");
+        System.out.println("Width of file: " + image.getWidth() + " pixels");
+        System.out.println("Height of file: " + image.getHeight() + " pixels");
+        System.out.println("BitsPerPixel: " + image.getColorModel().getPixelSize() + " bits");
+
+        int compression = image.getType();
         if (compression == BufferedImage.TYPE_BYTE_BINARY || compression == BufferedImage.TYPE_BYTE_INDEXED) {
             System.out.println("Compression status: true");
         } else {
             System.out.println("Compression status: false");
         }
-        System.out.println("Total value of colors: " + colorsCount);
+        System.out.println("Total value of colors: " + colorsCounter(image));
 
         int colorModel = image.getColorModel().getColorSpace().getType();
         System.out.println("Color model:" + (colorModel == 5 ? " Indexed" : colorModel == 6 ? " Direct" : " Unknown"));
-
-
     }
 
-    public static int byteArrayToInt(byte[] bytes, int offset) {
-        return (bytes[offset + 3] & 0xFF) << 24 |
-                (bytes[offset + 2] & 0xFF) << 16 |
-                (bytes[offset + 1] & 0xFF) << 8 |
-                bytes[offset] & 0xFF;
+    public static int colorsCounter(BufferedImage image) {
+        HashSet<Integer> colors = new HashSet<>();
+        for (int i = 0; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
+                int pixel = image.getRGB(j, i);
+                colors.add(pixel);
+            }
+        }
+        return colors.size();
     }
+
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
@@ -84,7 +73,6 @@ public class Main {
         System.out.println(listFiles(resourcesPath));
         System.out.print("Choose one: ");
         String filePath = resourcesPath + scanner.next();
-
 
         if (filePath.substring(filePath.lastIndexOf(".") + 1).equals("bmp")) {
             BufferedImage image = ImageIO.read(new File(filePath));
